8e39bed6c8443a5b0226b32e0fb94cfa58efb67e
Tz-Huan Huang <tzhuan@potentia.asia>
2023-12-22 16:40:15 +0800

    Release 8.0.0

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { default as pino } from 'pino'
+
+type Transport = {
+  type: 'gchat'
+  link: string
+  level: pino.Level
+}
+
+type Options = {
+  name: string
+  level?: pino.Level
+  transports?: Transport[]
+}
+
+export function createLogger(options: Options): ReturnType<typeof pino> {
+  const { level = 'info', transports = [], ...misc } = options
+  const targets: pino.TransportTargetOptions[] = transports.map(
+    ({ type, link, level }) => {
+      switch (type) {
+        case 'gchat':
+          return { target: './gchat.js', level, options: { link } }
+        default:
+          throw new Error(`Unsupported logger transport type ${type}`)
+      }
+    },
+  )
+  targets.push({
+    target: 'pino-pretty',
+    level: 'trace',
+    options: { destination: 1 },
+  })
+  return pino({ level, ...misc }, pino.transport({ targets }))
+}
